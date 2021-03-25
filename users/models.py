6b0feec0eac59ce1e24abfdc4790ae91555d2acb
iinsue <hinsu93@naver.com>
2021-03-25 12:18:42 +0900

    #3.3 finishing user model

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    """ Custom User Model """ #docstring
+    """ Custom User Model """#docstring
 
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
@@ -16,6 +16,21 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True) # blank는 필수입력 해제
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices = LANGUAGE_CHOICES, max_length = 2, null=True, blank=True)
+    currency = models.CharField(choices = CURRENCY_CHOICES, max_length = 3, null=True, blank=True)
+
+    superhost = models.BooleanField(default=False)
