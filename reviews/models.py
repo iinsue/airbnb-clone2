198af2219094ac83442fbea1885463e3746da017
iinsue <hinsu93@naver.com>
2021-04-12 11:21:00 +0900

    #8.1 Average & resevation admin

--- reviews/models.py
+++ reviews/models.py
@@ -9,7 +9,7 @@ class Review(core_models.TimeStampedModel):
     review = models.TextField()
     accuracy = models.IntegerField()
     communication = models.IntegerField()
-    cleanliness = models.IntegerField()
+    cleanness = models.IntegerField()
     location = models.IntegerField()
     check_in = models.IntegerField()
     value = models.IntegerField()
@@ -18,3 +18,16 @@ class Review(core_models.TimeStampedModel):
 
     def __str__(self):
         return f"{self.review} - {self.room}"
+
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg,2)
+
+    rating_average.short_description = "Avg"
